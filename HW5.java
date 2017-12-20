21059b48c98a4d154cac53092ba53766512e6d62
dils12001 <32242875+dils12001@users.noreply.github.com>
2017-12-20 17:49:45 +0800

    1220

--- HW5.java
+++ HW5.java
@@ -10,4 +10,5 @@ public class HW5 {
 		
 		tbl.play();
 	}
+
 }
--- Table.java
+++ Table.java
@@ -19,7 +19,7 @@ public class Table {
 	public Player[] get_player() {
 		return players;
 	}
-
+	
 	public void set_dealer(Dealer d) {
 		dealer = d;
 	}
@@ -36,7 +36,6 @@ public class Table {
 	}
 
 	private void distribute_cards_to_dealer_and_players() {
-		int a = 0;
 		for (Player p : get_player()) {
 			ArrayList<Card> temp = new ArrayList<Card>();
 			temp.add(allcards.getOneCard(true));
@@ -47,69 +46,102 @@ public class Table {
 		temp1.add(allcards.getOneCard(true));
 		temp1.add(allcards.getOneCard(false));
 		dealer.setOneRoundCard(temp1);
+		System.out.print("Dealer's face up card is ");
+		dealer.getOneRoundCard().get(1).printCard();;
 	}
 
 	private void ask_each_player_about_hits() {
+		
 		for (Player p : get_player()) {
+			ArrayList<Card> temp2 = p.getOneRoundCard();
 			boolean hit = false;
-			do {if(p.getTotalValue()>21)
-				break;
+			do {
+				if (p.getTotalValue() > 21)
+					break;
+				System.out.println("fuck"+p.getTotalValue()+p.hit_me(this));
 				hit = p.hit_me(this); // this
 				if (hit) {
-					ArrayList<Card> temp2 = new ArrayList<Card>();
-					System.out
-							.println("Hit! " + p.getName() + "・s cards now: ");
+					
+					System.out.println("Hit! " + p.getName() + "・s cards now: ");
 					temp2.add(allcards.getOneCard(true));
 					p.setOneRoundCard(temp2);
 					for (Card c : p.getOneRoundCard()) {
 						c.printCard();
 					}
-				} else {
-					System.out.println(p.getName() + ", Pass hit!");
-					for (Card c : p.getOneRoundCard()) {
-						c.printCard();
-					}
-				}
+				} 
 
 			} while (hit);
+			System.out.println(p.getName() + ", Pass hit!");
+					
+					
 		}
 	}
 
-	private void ask_dealer_about_hits(){
+	private void ask_dealer_about_hits() {
 		boolean hit = false;
-		do {
-			hit = dealer.hit_me(this); // this
+		ArrayList<Card> temp3 = dealer.getOneRoundCard();
+		do {System.out.print(dealer.hit_me(this));
+			hit = dealer.hit_me(this); 
 			if (hit) {
-				ArrayList<Card> temp3 = new ArrayList<Card>();
 				temp3.add(allcards.getOneCard(true));
 				dealer.setOneRoundCard(temp3);
-				} else {
-				System.out.println("Dealer's hit is over!");
-			}
+			} 
 		} while (hit);
+		System.out.println("Dealer's hit is over!");
 	}
-	
-	private void calculate_chips(){
-		
-		System.out.println("Dealer's card value is "+dealer.getTotalValue()+" ,Cards:");
+
+	private void calculate_chips() {
+		int d = dealer.getTotalValue();
+		System.out.println("Dealer's card value is " + d + " ,Cards:");
 		for (Card c : dealer.getOneRoundCard()) {
 			c.printCard();
 		}
+		for (Player player : get_player()) {
+			int win = 0;// キも0 怪長1 �営�2
+			int p = player.getTotalValue();
+			System.out.print(player.getName() + " card value is " + p);
+			if (p > 21)
+				win = 1;
+			else if (p <= 21 & d > 21)
+				win = 2;
+			else if (p <= 21 & d <= 21) {
+				if (p > d)
+					win = 2;
+				if (p < d)
+					win = 1;
+				if (p == d)
+					win = 0;
+			}
+			if (win == 1){
+				player.increaseChips(-player.makeBet());
+				System.out.println(", Loss " + player.makeBet()
+						+ " Chips, the Chips now is: "
+						+ player.getCurrentChips());
+				}
+			if (win == 2){
+				player.increaseChips(player.makeBet());
+				System.out.println(",Get " + player.makeBet()
+						+ " Chips, the Chips now is: "
+						+ player.getCurrentChips());}
+			if (win == 0)
+				System.out.println(",chips have no change! The Chips now is: "
+						+ player.getCurrentChips());
+		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public int[] get_palyers_bet() {
+		pos_betArray = new int[get_player().length];
+		for (int i = 0; i < get_player().length; i++)
+			pos_betArray[i] = players[i].makeBet();
+		return pos_betArray;
+	}
+
+	public void play() {
+		ask_each_player_about_bets();
+		distribute_cards_to_dealer_and_players();
+		ask_each_player_about_hits();
+		ask_dealer_about_hits();
+		calculate_chips();
+	}
+
 }
